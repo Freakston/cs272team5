46ff75cbf07e971087ec520378ebd9cf88202f5d
Suraj K Suresh <kssuraj15@gmail.com>
2024-02-12 18:06:20 -0800

    Testing new action

--- Dockerfile
+++ Dockerfile
@@ -12,3 +12,4 @@ COPY fuzz.sh /fuzz.sh
 
 RUN chmod +x /fuzz.sh
 
+ENTRYPOINT [ "/fuzz.sh" ]
--- fuzz.sh
+++ fuzz.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 
 # Check if the argument is provided
 if [ $# -eq 0 ]; then
@@ -9,6 +9,7 @@ fi
 # Assign the argument to a variable
 input_string="$1"
 
+echo "input=$input_string" >> $GITHUB_OUTPUT
 # Compare the input string with another string
 if [ "$input_string" = "libass" ]; then
     echo "Fuzzing libass"
