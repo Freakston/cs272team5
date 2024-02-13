24c998c98354598e1139abd63c41ad7fa5caf72b
Suraj K Suresh <kssuraj15@gmail.com>
2024-02-12 18:12:28 -0800

    Finish Fuzz-Pipeline

--- fuzz.sh
+++ fuzz.sh
@@ -6,14 +6,38 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Assign the argument to a variable
-input_string="$1"
+fuzz_project() {
+    input_string="$1"
 
-echo "input=$input_string" >> $GITHUB_OUTPUT
-# Compare the input string with another string
-if [ "$input_string" = "libass" ]; then
-    echo "Fuzzing libass"
-    /AFLplusplus/afl-fuzz -i /libass/seeds -D -o /libass/out -x /libass/fuzz/ass.dict  -- /libass/fuzz/fuzz
-else
-    echo "Failed to find the project to fuzz"
-fi
+    # Compare the input string with another string
+    if [ "$input_string" = "libass" ]; then
+        echo "Fuzzing libass"
+        /AFLplusplus/afl-fuzz -i /libass/seeds -D -o /libass/out -x /libass/fuzz/ass.dict -- /libass/fuzz/fuzz
+    else
+        echo "Failed to find the project to fuzz"
+    fi
+}
+
+fuzz_project "$1" &
+
+postprocess_results() {
+    /AFLplusplus/afl-whatsup -s -d /libass/out 
+}
+
+# Capture the PID of the background process
+pid=$!
+
+# Sleep for a short duration to allow the parallel process to start
+sleep 5
+
+# Main process sleeps for x minutes
+sleep "$(($2 * 60))"
+
+# Kill the parallel process
+echo "Killing AFL++ process PID $pid"
+
+kill "$pid"
+
+echo "Main process is done."
+
+postprocess_results
